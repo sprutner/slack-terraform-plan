af3358cad4cb8e69b2b2e6654a2321f6cc730386
Seth Rutner <srutner@life360.com>
2018-09-17 12:38:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tool to notify a slack channel of your terraform plans
+
+## Use case: Submitting plans to #server automatically
---
+++ slack_channel_notifier.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+from slackclient import SlackClient
+from python_terraform import *
+
+# INIT
+slack_token = os.environ["SLACK_API_TOKEN"]
+sc = SlackClient(slack_token)
+# Get current PWD
+cwd = os.getcwd()
+
+def init_terraform(tf_state_directory):
+    tf = Terraform(working_dir=tf_state_directory)
+    return tf
+
+def run_terraform_plan(tf):
+    return_code, stdout, stderr = tf.plan(no_color=IsFlagged)
+    return stdout
+
+def post_message(slack_channel, message_text):
+    sc.api_call(
+      "chat.postMessage",
+      channel=slack_channel,
+      text=message_text
+    )
+
+def upload_snippet(slack_channel, snippet):
+    sc.api_call(
+        "files.upload",
+        channels=slack_channel,
+        file=snippet,
+        title="Terraform Plan"
+    )
+
+if __name__ == "__main__":
+    print(cwd)
+    tf = init_terraform(cwd)
+    run_terraform_plan(tf)
+    # post_message('slackbottesting', run_terraform_plan(tf))
+    upload_snippet('slackbottesting', run_terraform_plan(tf))
